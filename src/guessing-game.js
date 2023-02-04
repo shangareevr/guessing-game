83aca89602f06f214577bafd87e5b5507d16bdd7
sh-rusel <sh.rusell@gmail.com>
2023-02-04 18:58:16 +0500

    feat:execute tast

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.midl = this.max-Math.floor((this.max - this.min) / 2);
+        return this.midl
     }
 
     lower() {
-
+        this.max= this.midl;
     }
 
     greater() {
-
+        this.min = this.midl;
     }
 }
 
